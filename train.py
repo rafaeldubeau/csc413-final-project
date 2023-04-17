5649e9a3ffda9218008257b5f068135ea55d53d8
WillGotlib <37600983+WillGotlib@users.noreply.github.com>
2023-04-17 14:40:57 -0400

    merged some stuff from my branch

--- train.py
+++ train.py
@@ -1,10 +1,19 @@
 import os
+import numpy as np
 
 import torch
+import torchvision
 from torch import nn
 from torch.optim import Adam
 from torchvision.io import read_image
 from torchvision.models import resnet50, ResNet50_Weights
+from torchvision import datasets
+from torchvision import transforms as transforms
+from torchvision.io import read_image
+from torch.utils.data import Dataset, DataLoader
+
+
+import matplotlib.pyplot as plt
 
 from networks import ConvClassifier
 
@@ -41,29 +50,29 @@ def train_epoch(model, data, optimizer, loss_fn, device):
             
 
 def trainUNet(names: list[str], epochs: int, starting_epoch: int, lipschitz: bool):
-
     # Hyperparameters
     learning_rate = 10e-4
     batch_size = 512
     alpha = 10e-6
 
     # Load dataset
-    # TODO: Fill in.
+    dataset = datasets.GTSRB(root= "./model", download=True, transform=transforms.ToTensor())
+    data_loader = torch.utils.data.DataLoader(dataset, shuffle=True)
 
     # Loss function.
     base_loss = nn.MSELoss()
     
     def UNetLoss(input, target):
-        
+        pass
 
     # loss_fn = lambda pred, y, model: base_loss(pred.squeeze(), y.squeeze()) + alpha * model.get_lipschitz_bound()
 
     optimizer = Adam(model.parameters(), lr=learning_rate)
 
-
     # Train loop
     for t in range(epochs):
         print(f"Epoch {t+1}\n-------------------------------")
+        train_epoch(model, )
         train_loop(model, train_dataloader, optimizer, loss_fn)
         eval_loop(model, test_dataloader, loss_fn)
         if (t+1) % 5 == 0:
@@ -102,7 +111,3 @@ def train_copycat():
         train_epoch(model, train_dataloader, optimizer, loss_fn)
         if (e+1) % 5 == 0:
             torch.save(model.state_dict(), os.path.join("data", "models", f"copycat_{e+1}.pth"))
-
-
-
-
