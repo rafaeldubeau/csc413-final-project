b5d46eed2637449c188081dfa4dd0325962a3779
Rafael Dubeau <rafdubeau@gmail.com>
2023-04-19 14:24:15 -0400

    trained copycat network to 99.5% accuracy

--- train_copycat.py
+++ train_copycat.py
@@ -115,26 +115,36 @@ if __name__ == "__main__":
 
     # Hyperparameters
     epochs = 5
+    starting_epoch = 5
     learning_rate = 1e-3
-    batch_size = 64
+    batch_size = 128
     weight_decay = 1e-2
 
     # Load Dataset
+    
     train_dataset = InferrenceDataset()
-    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-    test_dataset = InferrenceDataset(split="test")
-    test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
+    train_size = int(len(train_dataset) * 0.85)
+    val_size = int(len(train_dataset) * 0.15)
+    train_set, val_set = torch.utils.data.random_split(train_dataset, (train_size, val_size)) 
+    train_dataloader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
+    val_dataloader = DataLoader(val_set, batch_size=batch_size, shuffle=True)
 
     # Initialize Model
     model = ConvClassifier(3, 43).to(device)
+    if starting_epoch > 0:
+        model.load_state_dict(torch.load(os.path.join("data", f"copycat_{starting_epoch}.pth")))
     optimizer = Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
 
     # Loss Function
     loss_fn = nn.CrossEntropyLoss()
 
     for e in range(epochs):
+        print(f"Epoch {starting_epoch+e+1}\n-------------------------------")
         train_epoch(model, train_dataloader, optimizer, loss_fn, device)
-        evaluate(model, test_dataloader, loss_fn, device)
+        evaluate(model, val_dataloader, loss_fn, device)
+
+        if (e+1) % 5 == 0:
+            torch.save(model.state_dict(), os.path.join("data", f"copycat_{starting_epoch+e+1}.pth"))
 
 
 
